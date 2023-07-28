9cd8534280b21fa16eb5f0b54dc0a631469bf2c1
Raxita Mathukiya <jay343421@gmail.com>
2023-07-28 19:07:05 +0530

    done

--- index.js
+++ index.js
@@ -5,26 +5,19 @@ require("dotenv").config();
 const {doctorRouter}=require("./Routes/doctor.routes");
 const { setupSocket } = require('./socket'); 
 const cors = require('cors');
-const passport = require("passport")
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(passport.initialize())
 const server = require('http').createServer(app);
 
 setupSocket(server); 
 
 
-  app.get('/',async(req,res)=>{
-    try {
-        res.send("welcome to home page")
-    } catch (error) {
-        console.log(error)
-    }
-  })
+  
 app.use("/doctor",doctorRouter)
 
-app.use('/',userRoutes)
+app.use('/user',userRoutes)
 // 1) userRoutes
 // 2) doctor Routes
 
